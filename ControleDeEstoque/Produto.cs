0a3186d9478c1540bb8bad8db3cbb6ba23b58271
Augusto Mattevi <razanidark@gmail.com>
2024-03-01 11:22:27 -0300

    Atualização

--- ControleDeEstoque/Produto.cs
+++ ControleDeEstoque/Produto.cs
@@ -23,10 +23,11 @@ namespace ControleDeEstoque
             Quantidade -= quantidade;
         }
 
-        public void AdicionarValor (double taxa)
+        public void AdicionarValor(double taxa)
         {
             Preco += Preco * (taxa / 100);
         }
+
         public override string ToString() //override (sobreposicao) explicado no capitulo de heranca
         {
             return Nome
