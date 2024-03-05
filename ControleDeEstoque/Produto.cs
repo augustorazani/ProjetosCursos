bf0f77aab969ed02ba84acca2de74569f36da059
Augusto Mattevi <razanidark@gmail.com>
2024-03-05 16:05:06 -0300

    Iniciando integração e consulta em DB

--- ControleDeEstoque/Produto.cs
+++ ControleDeEstoque/Produto.cs
@@ -23,6 +23,11 @@ namespace ControleDeEstoque
             Quantidade -= quantidade;
         }
 
+        public void AdicionarValor(double taxa)
+        {
+            Preco += Preco * (taxa / 100);
+        }
+
         public override string ToString() //override (sobreposicao) explicado no capitulo de heranca
         {
             return Nome
