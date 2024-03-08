985667a66663139313b0423bacd4ad033f11f457
Augusto Mattevi <razanidark@gmail.com>
2024-03-08 17:44:49 -0300

    Atualizações

---
+++ Ex2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f = new Funcionario();
+
+            Console.Write("Nome: ");
+            f.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Funcionário: " + f);
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double porcentagem = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            f.AumentarSalario(porcentagem);
+
+            Console.Write("Dados atualizados: " + f);
+            Console.ReadLine();
+
+        }
+    }
+}
