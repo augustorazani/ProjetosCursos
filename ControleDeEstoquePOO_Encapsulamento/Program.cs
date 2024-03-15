e9208a19818059b81e46d22aff493ddfa0dab6d7
Augusto Mattevi <143201353+augustorazani@users.noreply.github.com>
2024-03-14 21:37:58 -0300

    teste (#11)

---
+++ ControleDeEstoquePOO_Encapsulamento/Program.cs
@@ -0,0 +1,25 @@
+﻿using ControleDeEstoque;
+using System;
+using System.Globalization;
+
+namespace ControleDeEstoquePOO_Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.Nome = "T"; //conforme o if programado na propriedade SET da classe Produto,
+                          //o atributo Nome permanecerá TV pois foi instanciado dessa forma na linha 11 (para setar, é necessário value.Length > 1)
+
+            p.Nome = "T";
+
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Ex3MembrosEstaticos/ConversorDeMoeda.cs
@@ -0,0 +1,13 @@
+﻿namespace Ex3MembrosEstaticos
+{
+    internal class ConversorDeMoeda
+    {
+        public static double iof = 6.0;
+
+        public static double CalcularReais(double cotacao, double qtd)
+        {
+            double total = cotacao * qtd;
+            return total + total * (iof/100.0);
+        }
+    }
+}
