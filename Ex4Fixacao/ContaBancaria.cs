c1851fc80145e4f8f5e8b49cb2ee11ed1d8c9720
Augusto Mattevi <razanidark@gmail.com>
2024-03-21 18:38:54 -0300

    Update 21/03

---
+++ Ex4Fixacao/ContaBancaria.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace Ex4Fixacao
+{
+    internal class ContaBancaria
+    {
+        public int NumeroConta { get; set; }
+        public string NomeTitular { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria() { }
+        public ContaBancaria(int numeroConta, string nomeTitular, double saldo )
+        {
+            NumeroConta = numeroConta;
+            NomeTitular = nomeTitular;
+            Saldo = saldo;
+        }
+
+
+        public void DepositarSaldo (double saldo)
+        {
+            Saldo += saldo;
+        }
+        public override string ToString()
+        {
+            return "Conta " +
+                NumeroConta +
+                ", Titular: " +
+                NomeTitular +
+                ", Saldo: $ " + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Ex4Fixacao/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex4Fixacao
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            /*
+            classe conta
+            atributos:
+            NumeroConta = get private set
+            NomeTitular = get set
+            Saldo = pode ser zero e negativo, private get private set
+
+            metodos:
+            DepositarSaldo void double saldo
+            SaqueSaldo void double saldo + $5,00/cada saque
+            override ToString para obter os dados atuais da conta
+            */
+
+            ContaBancaria conta = new ContaBancaria();
+
+            Console.Write("Entre o número da conta: ");
+            conta.NumeroConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            conta.NomeTitular = Console.ReadLine();
+
+            char deposita;
+            do
+            {
+                Console.Write("Haverá depósito inicial (s/n)? ");
+                deposita = char.Parse(Console.ReadLine());
+                switch (deposita)
+                {
+                    case 's':
+                        Console.WriteLine("Entre o valor de depósito inicial: ");
+                        conta.DepositarSaldo(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+                        Console.WriteLine("Dados da conta:");
+                        Console.WriteLine(conta);
+                        break;
+
+                    case 'n':
+                        Console.WriteLine("Dados da conta:");
+                        Console.WriteLine(conta);
+                        break;
+
+                    default:
+                        Console.WriteLine("Digite uma opção válida!!");
+                        break;
+                }
+            }
+            while (deposita != 's' && deposita != 'n');
+            Console.ReadLine();
+        }
+    }
+}
