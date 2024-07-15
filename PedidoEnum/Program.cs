7dfce47c0c90f62f7ff8362c27ad1b1849008800
Augusto Mattevi <razanidark@gmail.com>
2024-07-14 22:20:21 -0300

    Composicao - Worker

--- PedidoEnum/Program.cs
+++ PedidoEnum/Program.cs
@@ -19,11 +19,12 @@ namespace PedidoEnum
                 Console.WriteLine(order);  //utiliza override para retornar dados
 
                 string txt = OrderStatus.PendingPayment.ToString(); //converte valor ENUM para String (atribuir o status PendingPaymente a var txt) 
-                OrderStatus os = (OrderStatus)Enum.Parse(typeof(OrderStatus), value: "Delivered");
+                OrderStatus os = (OrderStatus)Enum.Parse(typeof(OrderStatus), value: "Delivered", true);
                 //Enum.TryParse("Delivered", true, out os); <- forma alternativa funcional
                 //OrderStatus os = Enum.Parse<OrderStatus>("Delivered"); <- dessa forma nao funcionou
-                Console.WriteLine(os);
                 Console.WriteLine(txt);
+                Console.WriteLine(os);
+
                 Console.ReadLine();
             }
             catch (ArgumentException ex)
